e85da47a6c4b480adb30c70b141e6aa9b262bbb8
Dhananjay <dhananjayakumar1314@gmail.com>
2022-11-24 19:33:32 +0530

    change

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'BASE_URL_ADDRESS';
+    public $baseURL = BASE_URL_ADDRESS;
 
     /**
      * --------------------------------------------------------------------------
