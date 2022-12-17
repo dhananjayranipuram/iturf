669eeccac561d1f7012eff40f71b685b68a8e410
Dhananjay <dhananjayakumar1314@gmail.com>
2022-12-17 21:43:01 +0530

    t

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -100,8 +100,8 @@ switch ($host) {
         $baseUrl = 'http://iturf.local/';
         break;
     default:
-        $baseUrl = $_SERVER['HTTP_HOST'].'/public/';
+        $baseUrl = 'http://'.$_SERVER['HTTP_HOST'].'/public/';
         break;
 }
-echo $baseUrl;exit;
+// echo $baseUrl;exit;
 define('BASE_URL_ADDRESS', $baseUrl);
