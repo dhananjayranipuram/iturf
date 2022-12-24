b9b9566178886b9b617418e33bd4ace2c1f44ba3
Dhananjay <dhananjayakumar1314@gmail.com>
2022-12-24 19:19:28 +0530

    t

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -100,7 +100,7 @@ switch ($host) {
         $baseUrl = 'http://iturf.local/';
         break;
     default:
-        $baseUrl = "http://iturf.co.in/index.php/";
+        $baseUrl = "http://iturf.co.in/";
         break;
 }
 // echo $baseUrl;exit;
